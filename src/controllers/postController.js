9486e45480a967726112ba7d7c6f402cd146b72f
messiasGeovani <messias.geovani.lima@gmail.com>
2019-11-26 18:22:27 +0000

    participants config

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -4,9 +4,10 @@ const path = require('path');
 const fs = require('fs');
 
 module.exports = {
-    async index(req, res){
+    async index(req, res) {
         // retorna os posts por data de criação
-        const posts = await Post.find().sort({'createdAt': -1}).populate('user');
+        const posts = await Post.find().sort({ 'createdAt': -1 }).populate('user');
+        console.log(posts)
 
         return res.json(posts);
     },
@@ -20,41 +21,41 @@ module.exports = {
         }
     },
 
-    async store(req, res){
+    async store(req, res) {
         try {
-            const {postTitle, date, placeEvent, description, participants } = req.body;
-            const {filename: image} = req.file;
+            const { postTitle, date, placeEvent, description, participants } = req.body;
+            const { filename: image } = req.file;
             // separando imagem em nome e extensão
             const [name] = image.split('.');
             const fileName = `${name}.jpg`;
-    
+
             // redimensiona a imagem do post para aumento de perfomance
             await sharp(req.file.path)
                 .resize(500)
-                .jpeg({quality:70})
+                .jpeg({ quality: 70 })
                 .toFile(
                     path.resolve(req.file.destination, 'resized', fileName)
                 )
             // apaga a imagem original    
-            fs.unlinkSync(req.file.path); 
-    
+            fs.unlinkSync(req.file.path);
+
             const post = await Post.create({
                 postTitle,
                 description,
-                date, 
+                date,
                 placeEvent,
                 image: fileName,
                 user: req.userId,
                 participants: req.userId,
             });
-            
+
             // Socket IO = Compartilhar Informações em tempo real
             req.io.emit('post', post)
-    
+
             return res.json(post)
-        } catch (err){
+        } catch (err) {
             console.log(err)
-            return res.status(400).send({error: 'Failed create new Post'})
+            return res.status(400).send({ error: 'Failed create new Post' })
         }
     },
 
@@ -63,8 +64,8 @@ module.exports = {
 
             await Post.findByIdAndRemove(req.params.postId)
 
-            return res.status(200).send({message: 'Sucess delete'})
-            
+            return res.status(200).send({ message: 'Sucess delete' })
+
         } catch (err) {
             return res.status(400).send({ error: 'Error deleting post' })
         }
@@ -72,26 +73,36 @@ module.exports = {
 
     async update(req, res) {
         try {
-            const {participants} = req.body
-            
-            const post = await Post.findByIdAndUpdate(req.params.postId,{
+            const post = await Post.findByIdAndUpdate(req.params.postId, {
                 postTitle,
                 description,
-            }, {new: true}).populate('user')
-
-            await Promise.all(participants.map( async participants =>{
-                const eventParticipant = new Participant({ ...participants, post: _id})
-                await eventParticipant.save()
-                post.participants.push(eventParticipant)
-            }))
+            }, { new: true }).populate('user')
 
             await post.save()
 
-            return res.send({post})
-            
+            return res.send({ post })
+
         } catch (err) {
             console.log(err)
             return res.status(400).send({ error: 'Error update post' })
+        }
+    },
+
+    async participate(req, res) {
+        try {
+            const { userId, postId } = req.body
+
+            const post = await Post.findById(postId).populate('user')
+            
+            if (!post.participants.includes(userId)) {
+                post.participants.push(userId)
+            }
+
+            return res.status(200).send(post)
+
+        } catch (error) {
+            console.log(error)
+            return res.status(500).send(error)
         }
     }
 }
